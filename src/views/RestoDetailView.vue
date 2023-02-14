70c931e55ff731a642d81e695de5a3f547b44e4f
derruu <norocanid@gmail.com>
2023-02-14 09:53:47 +0700

    refactor(card): make card reusable

--- src/views/RestoDetailView.vue
+++ src/views/RestoDetailView.vue
@@ -1,44 +1,25 @@
 <script setup>
 import { useRestoRepository } from "@/composables";
 import { ref, onMounted } from "vue";
-import { RouterLink } from "vue-router"; 
-
+import { useRoute } from "vue-router";
 const repository = useRestoRepository();
+const route = useRoute();
 const isLoading = ref(true);
-const restos = ref([]);
-const fetchRestos = async () => {
+const detail = ref({});
+const fetchDetail = async () => {
   isLoading.value = true;
   try {
-    const { data } = await repository.index();
-    restos.value = data;
+    const id = route.params.id;
+    const { data } = await repository.show(id);
+    detail.value = data;
   } catch (e) {
-    console.error(e);
+    console.log(e);
   }
   isLoading.value = false;
 };
-onMounted(() => fetchRestos());
-const excerpt = (text, maxLenght = 10, indicator = "...") => {
-  let textCopy = text;
-  if (textCopy.length > maxLenght) {
-    textCopy = textCopy.substring(0, maxLenght) + indicator;
-  }
-  return textCopy;
-};
+onMounted(() => fetchDetail());
 </script>
 
 <template>
-  <div class="min-h-screen container mx-auto">
-    <div class="grid grid-cols-12 gap-4 py-4">
-      <div v-for="resto in restos" :key="resto.id" class="col-span-4">
-        <!-- Card -->
-        <RouterLink
-          :to="{ name: 'restos-show', params: { id: resto.id } }"
-          class="block bg-white shadow p-4 rounded select-none"
-        >
-          <h1 class="text-lg font-bold capitalize">{{ resto.name }}</h1>
-          <p class="text-gray-500">{{ excerpt(resto.description, 40) }}</p>
-        </RouterLink>
-      </div>
-    </div>
-  </div>
+  <div>Sedang menampilkan resto dengan ID: {{ route.params.id }}</div>
 </template>
